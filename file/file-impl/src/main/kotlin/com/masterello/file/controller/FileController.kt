82d3717acec45a2362a283fa33610ce36d35f9a0
Igor Sila <silaigor94@gmail.com>
2024-10-14 00:15:23 +0200

    updated file with security configs, updated kotlin version

--- file/file-impl/src/main/kotlin/com/masterello/file/controller/FileController.kt
+++ file/file-impl/src/main/kotlin/com/masterello/file/controller/FileController.kt
@@ -1,5 +1,8 @@
 package com.masterello.file.controller
 
+import com.masterello.auth.data.AuthZRole
+import com.masterello.commons.security.validation.AuthZRule
+import com.masterello.commons.security.validation.AuthZRules
 import com.masterello.file.dto.FileDto
 import com.masterello.file.service.FileService
 import com.masterello.file.util.FileUtil
@@ -21,6 +24,10 @@ class FileController {
     @Autowired
     private lateinit var fileService: FileService
 
+    @AuthZRules(
+        AuthZRule(roles = [AuthZRole.USER, AuthZRole.WORKER], isOwner = true),
+        AuthZRule(roles = [AuthZRole.ADMIN])
+    )
     @GetMapping("/{userUuid}")
     @Operation(summary = "Get all files by user UUID", description = "Retrieve all files for a specific user")
     @ApiResponse(responseCode = "200", description = "Successfully retrieved files")
@@ -29,6 +36,10 @@ class FileController {
         return ResponseEntity.ok(files)
     }
 
+    @AuthZRules(
+        AuthZRule(roles = [AuthZRole.USER, AuthZRole.WORKER], isOwner = true),
+        AuthZRule(roles = [AuthZRole.ADMIN])
+    )
     @Operation(summary = "Upload user file", description = "Upload user file")
     @ApiResponse(responseCode = "200", description = "Creates user file and uploads it to the server")
     @PostMapping("/upload", consumes = ["multipart/form-data"])
@@ -38,6 +49,10 @@ class FileController {
         return ResponseEntity.ok("created file")
     }
 
+    @AuthZRules(
+        AuthZRule(roles = [AuthZRole.USER, AuthZRole.WORKER], isOwner = true),
+        AuthZRule(roles = [AuthZRole.ADMIN])
+    )
     @Operation(summary = "Delete user file", description = "Delete user file")
     @ApiResponse(responseCode = "204", description = "Deletes user file and removes it from the server")
     @DeleteMapping("/{userUuid}/{fileUuid}")
@@ -50,10 +65,13 @@ class FileController {
         }
     }
 
-
+    @AuthZRules(
+        AuthZRule(roles = [AuthZRole.USER, AuthZRole.WORKER], isOwner = true),
+        AuthZRule(roles = [AuthZRole.ADMIN])
+    )
     @GetMapping("/{userUuid}/{fileUuid}")
-    @Operation(summary = "Get all files by user UUID", description = "Retrieve all files for a specific user")
-    @ApiResponse(responseCode = "200", description = "Successfully retrieved files")
+    @Operation(summary = "Download file by user UUID and file uuid", description = "Download file or user by file uuid")
+    @ApiResponse(responseCode = "200", description = "Successfully downloads file")
     fun downloadFile(@PathVariable userUuid: UUID, @PathVariable fileUuid: UUID) : ResponseEntity<ByteArray> {
         val fileData = fileService.downloadUserFile(userUuid, fileUuid)
 
