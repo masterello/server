0363129259e624f064b7992c47fda0c79178e6b2
Igor Sila <silaigor94@gmail.com>
2024-10-14 21:30:53 +0200

    updated filter config

--- file/file-impl/src/main/kotlin/com/masterello/file/configuration/FileSecurityConfig.java
+++ file/file-impl/src/main/kotlin/com/masterello/file/configuration/FileSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.masterello.file.configuration;
 
+import com.masterello.commons.security.filter.AuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +9,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class FileSecurityConfig {
 
+    private final AuthFilter authFilter;
 
     @Bean
     public SecurityFilterChain apiAuthFilter(HttpSecurity http) throws Exception {
@@ -21,7 +24,8 @@ public class FileSecurityConfig {
                 .securityMatcher("/api/files", "/api/files/**")
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll())
+                        .anyRequest().authenticated())
+                .addFilterBefore(authFilter, AnonymousAuthenticationFilter.class)
                 .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
