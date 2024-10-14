350b7da0da946483864ae01c53c3768f12551525
Igor Sila <silaigor94@gmail.com>
2024-10-14 23:58:21 +0200

    fixing github build

--- app/src/main/java/com/masterello/MasterelloServerApplication.java
+++ app/src/main/java/com/masterello/MasterelloServerApplication.java
@@ -1,15 +1,12 @@
 package com.masterello;
 
-import com.masterello.file.configuration.FileProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-@EnableConfigurationProperties(value = FileProperties.class)
 @ConfigurationPropertiesScan
 public class MasterelloServerApplication {
 
