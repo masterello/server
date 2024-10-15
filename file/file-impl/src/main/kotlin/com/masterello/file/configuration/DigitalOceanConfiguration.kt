de0e3851042aab120d390c2487102b25b5dc839d
Igor Sila <ihor.sila@deliveryhero.com>
2024-10-15 08:21:49 +0200

    file module included (#19)
    * file module included
    
    * added region to props
    
    ---------
    
    Co-authored-by: Igor Sila <silaigor94@gmail.com>

--- file/file-impl/src/main/kotlin/com/masterello/file/configuration/DigitalOceanConfiguration.kt
+++ file/file-impl/src/main/kotlin/com/masterello/file/configuration/DigitalOceanConfiguration.kt
@@ -5,18 +5,20 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.S3Configuration
 import java.net.URI
 
 @Configuration
-class DigitalOceanConfiguration(
+open class DigitalOceanConfiguration(
     @Value("\${DO_SPACES_ACCESS_KEY}") private val accessKey: String,
     @Value("\${DO_SPACES_SECRET_KEY}") private val secretKey: String,
-    @Value("\${DO_SPACES_ENDPOINT}") private val endpoint: String
+    @Value("\${DO_SPACES_ENDPOINT}") private val endpoint: String,
+    @Value("\${DO_SPACES_REGION}") private val region: String
 ) {
     @Bean
-    fun s3Client(): S3Client {
+    open fun s3Client(): S3Client {
         val credentials = AwsBasicCredentials.create(accessKey, secretKey)
         val s3Config = S3Configuration.builder()
             .pathStyleAccessEnabled(true)
@@ -26,6 +28,7 @@ class DigitalOceanConfiguration(
             .credentialsProvider(StaticCredentialsProvider.create(credentials))
             .endpointOverride(URI.create(endpoint))
             .serviceConfiguration(s3Config)
+            .region(Region.of(region))
             .build()
     }
 }
