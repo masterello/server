8d00f5f46042ad09149828d15fc96b5a19c74fd2
Dmitry Borodin <dmitry.borodin90@gmail.com>
2024-11-03 12:59:04 +0100

    Add authZ to categories API

--- category/category-impl/src/main/kotlin/com/masterello/category/configuration/CategorySecurityConfig.kt
+++ category/category-impl/src/main/kotlin/com/masterello/category/configuration/CategorySecurityConfig.kt
@@ -1,5 +1,6 @@
 package com.masterello.category.configuration
 
+import com.masterello.commons.security.filter.AuthFilter
 import kotlin.jvm.Throws;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,22 +8,38 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.OrRequestMatcher
+import org.springframework.security.web.util.matcher.RequestMatcher
 
 @Configuration
 @EnableWebSecurity
-open class CategorySecurityConfig {
+open class CategorySecurityConfig(private val authFilter: AuthFilter) {
 
     @Bean
     @Throws(Exception::class)
     open fun apiCategoryAuthFilter(http: HttpSecurity): SecurityFilterChain {
+
+        val protectedEndpoints: RequestMatcher = OrRequestMatcher(
+                AntPathRequestMatcher("/api/categories", "POST"),
+                AntPathRequestMatcher("/api/categories/{id}", "PUT"),
+                AntPathRequestMatcher("/api/categories/{id}/activate", "PUT")
+        )
+
         http
-            .securityMatcher(AntPathRequestMatcher("/api/categories/**"))
-            .csrf { it.disable() }
-            .authorizeHttpRequests { auth -> auth
-                .anyRequest().permitAll() }
-            .sessionManagement { session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+                .securityMatcher(AntPathRequestMatcher("/api/categories/**"))
+                .csrf { it.disable() }
+                .authorizeHttpRequests { auth ->
+                    auth
+                            .requestMatchers(protectedEndpoints).authenticated()  // Require authentication for specific endpoints
+                            .anyRequest().permitAll()
+                }
+                .addFilterBefore(authFilter, AnonymousAuthenticationFilter::class.java)
+                .sessionManagement { session ->
+                    session
+                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                }
 
         return http.build()
     }
