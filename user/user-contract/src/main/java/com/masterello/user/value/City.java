693edfde949b8961c11395eab8e31114ea33c7f0
Dmitrii_Borodin <dmitry.borodin90@gmail.com>
2025-06-09 13:09:05 +0200

    Add cities (#82)
    * extend cities
    
    * fix deprecated job
    
    * update codes to uppercase

--- user/user-contract/src/main/java/com/masterello/user/value/City.java
+++ user/user-contract/src/main/java/com/masterello/user/value/City.java
@@ -13,7 +13,23 @@ public enum City {
     BERLIN("BE"),
     HAMBURG("HH"),
     MUNICH("M"),
-    FRANKFURT("F");
+    KOELN("KO"),
+    FRANKFURT_AM_MAIN("F"),
+    DUESSELDORF("D"),
+    STUTTGART("S"),
+    LEIPZIG("L"),
+    DORTMUND("DO"),
+    BREMEN("HB"),
+    ESSEN("E"),
+    DRESDEN("DD"),
+    NUERNBERG("N"),
+    HANNOVER("H"),
+    DUISBURG("DU"),
+    WUPPERTAL("W"),
+    BOCHUM("BO"),
+    BIELEFELD("BI"),
+    BONN("BN"),
+    MANNHEIM("MA");
 
     @JsonValue
     private final String code;
@@ -23,8 +39,9 @@ public enum City {
         return Arrays.stream(City.values())
                 .filter(city -> city.getCode().equalsIgnoreCase(code))
                 .findAny()
-                .orElseThrow(() -> new IllegalArgumentException("Unknown city " + code ));
+                .orElseThrow(() -> new IllegalArgumentException("Unknown city " + code));
     }
 }
+
 
 
