ccc69027207d893f538ba867fe52d9bcdccdc6db
Dmitrii_Borodin <dmitry.borodin90@gmail.com>
2025-07-15 15:11:14 +0200

    fix redirect after google auth error (#96)

---
+++ auth/auth-impl/src/main/java/com/masterello/auth/responsehandlers/GoogleFailureAuthHandler.java
@@ -0,0 +1,32 @@
+package com.masterello.auth.responsehandlers;
+
+import com.masterello.auth.repository.CustomStatelessAuthorizationRequestRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GoogleFailureAuthHandler implements AuthenticationFailureHandler {
+
+    private final CustomStatelessAuthorizationRequestRepository authorizationRequestRepository;
+    @Value("${masterello.auth.google.success-redirect-url}")
+    private String googleSuccessRedirectUrl;
+    @Value("${masterello.auth.google.error-redirect-url}")
+    private String googleFailureRedirectUrl;
+
+    public GoogleFailureAuthHandler(CustomStatelessAuthorizationRequestRepository authorizationRequestRepository) {
+        this.authorizationRequestRepository = authorizationRequestRepository;
+    }
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        authorizationRequestRepository.removeCookie(request, response);
+        response.sendRedirect(googleFailureRedirectUrl + "?error=google_auth_failed");
+    }
+}
