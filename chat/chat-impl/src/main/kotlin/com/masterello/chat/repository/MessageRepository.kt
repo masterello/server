7d6890308ce27fe7b45e7914fe271d83cf400c45
Dmitrii_Borodin <dmitry.borodin90@gmail.com>
2025-10-07 21:20:44 +0200

    Improve chat history (#182)

--- chat/chat-impl/src/main/kotlin/com/masterello/chat/repository/MessageRepository.kt
+++ chat/chat-impl/src/main/kotlin/com/masterello/chat/repository/MessageRepository.kt
@@ -1,8 +1,7 @@
 package com.masterello.chat.repository
 
 import com.masterello.chat.domain.Message
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.time.OffsetDateTime
@@ -11,5 +10,5 @@ import java.util.*
 @Repository
 interface MessageRepository : JpaRepository<Message, UUID> {
 
-    fun findByChatIdAndCreatedAtBefore(chatId : UUID, before: OffsetDateTime, page: PageRequest) : Page<Message>
+    fun findByChatIdAndCreatedAtBefore(chatId : UUID, before: OffsetDateTime, pageable: Pageable) : List<Message>
 }
